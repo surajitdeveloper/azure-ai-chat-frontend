c0bc11ca7e963a9d5139c378f9b0a5c1043f3f04
Surajit <surajit1992@live.com>
2026-01-03 12:15:06 +0530

    revert

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 
 // const connectionUrl = "http://localhost:4000";
-const connectionUrl = "https://azure-ai-chat-backend.vercel.app/";
+const connectionUrl = "https://web-production-6ebb7.up.railway.app/";
 const socket = io.connect(connectionUrl);
 
 const App = () => {
